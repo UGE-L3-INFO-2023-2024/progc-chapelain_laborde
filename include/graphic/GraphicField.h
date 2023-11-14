14d9896d5c01cb9d11481508a01d924155a311fe
Nathan <nathan.chapelain@gmail.com>
2023-11-14 12:32:02 +0100

    Starting reflexion in graphic interface

---
+++ include/graphic/GraphicField.h
@@ -0,0 +1,15 @@
+#ifndef __GRAPHICFIELD_H__
+#define __GRAPHICFIELD_H__
+
+#include <MLV/MLV_all.h>
+
+#include "../Map.h"
+
+/**
+ * @brief Show the map
+ *
+ * @param map Map to show
+ */
+void showMap(Map map);
+
+#endif  // __GRAPHICFIELD_H__
---
+++ include/graphic/GraphicOverlay.h
@@ -0,0 +1,29 @@
+#ifndef __GRAPHIC_OVERLAY_H__
+#define __GRAPHIC_OVERLAY_H__
+
+#include "Gemstone.h"
+
+/**
+ * @brief Show the Mana bar
+ *
+ * @param x Coordinate x of the North West corner of the bar
+ * @param y Coordinate y of the North West corner of the bar
+ * @param w Width of the bar
+ * @param h Height of the bar
+ * @param max Max value of Mana
+ * @param current Current value of Mana
+ */
+void showManaBar(int x, int y, int w, int h, int max, int current);
+
+/**
+ * @brief Show inventory of gems
+ *
+ * @param x Coordinate x of the North West corner of the inventory
+ * @param y Coordinate y of the North West corner of the inventory
+ * @param width Width of the inventory
+ * @param gems Array of gems
+ * @param gemsCount Number of gems
+ */
+void showInventory(int x, int y, int width, Gem* gems, int gemsCount);
+
+#endif  // __GRAPHIC_OVERLAY_H__
